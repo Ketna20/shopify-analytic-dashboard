35183aca751b90f27c604cb57f5b159de575af01
ketna20 <ketna.kumar@gmail.com>
2026-03-25 20:41:44 -0700

    chore: clean duplicate backend files

--- src/main/java/com/ketna/shopify_analytics/exception/ShopifySyncException.java
+++
@@ -1,8 +0,0 @@
-package com.ketna.shopify_analytics.exception;
-
-public class ShopifySyncException extends RuntimeException {
-
-    public ShopifySyncException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/ketna/shopify_analytics/repository/StoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ketna.shopify_analytics.repository;
-
-import com.ketna.shopify_analytics.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
-
